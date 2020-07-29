24079a72a48a50b3dc60664cda7f492adf3271c8
Michael Cui <cuimichael500@gmail.com>
2020-07-29 14:33:57 -0500

    reformmated navbar added buy page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,10 +5,10 @@ var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  app.get("/", function(req, res) {
+  app.get("/index", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      return res.redirect("/members");
+      return res.redirect("/");
     }
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
@@ -16,39 +16,39 @@ module.exports = function(app) {
   app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      return res.redirect("/members");
+      return res.redirect("/");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/buy", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      return res.redirect("/members");
+    if (!req.user) {
+      return res.redirect("/login");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/buy.html"));
   });
 
   app.get("/sell", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      return res.redirect("/members");
+    if (!req.user) {
+      return res.redirect("/login");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/sell.html"));
   });
 
   app.get("/members", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      return res.redirect("/members");
+    if (!req.user) {
+      return res.redirect("/login");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      return res.redirect("/members");
+      return res.redirect("/");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
