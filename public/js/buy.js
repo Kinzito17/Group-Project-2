0876e385bf5df002cbd3cdc34f1b86d64bd5e5e4
Michael Cui <cuimichael500@gmail.com>
2020-07-30 10:50:44 -0500

    messing with card img

--- public/js/buy.js
+++ public/js/buy.js
@@ -13,12 +13,17 @@ $(document).ready(function() {
 
     for (var a = 0; a <data.length; a++){
 
-      var templateString = '<h2>' + (Object.values(data[a].plantName).join('')) + '</h2><p>' + Object.values(data[a].price).join('') + '</p><p>' + Object.values(data[a].description).join('') + '<img class = "card-img-top" src = '+ Object.values(data[a].imgURL).join('')+'>';
+      var img = + (Object.values(data[a].imgURL).join(''));
+
+      var templateString = '<h2>' + (Object.values(data[a].plantName).join('')) + '</h2><p>' + Object.values(data[a].price).join('') + '</p><p>' + Object.values(data[a].description).join('');
         html += `      
            <div class="col-4">
             <div class="card" style="width:20em;text-align:center;display:inline-block;">
+            <img class = "card-img-top" src = "`+img+`">
                  <div class="card-body">
                  `+templateString+`
+                 `+img+`
+                 <button type = input>Purchase
                 </div>
               </div>
         </div>`;
