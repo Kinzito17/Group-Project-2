405a4139c6f623a8689511b5b02cd79838579941
Matthew Kinzle <matt.kinzle@gmail.com>
2020-07-31 09:28:39 -0500

    favicon

--- routes/buy-routes.js
+++ routes/buy-routes.js
@@ -8,17 +8,32 @@ module.exports = function(app) {
     res.json(req.user);
   });
 
+  // app.delete("/api/buy/:id", function(req, res) {
+  //   // We just have to specify which todo we want to destroy with "where"
+  //   db.Plant.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function(dbPlant) {
+  //     res.json(dbPlant);
+  //   });
 
-  app.delete("/api/buy/:id", function(req, res) {
-    // We just have to specify which todo we want to destroy with "where"
-    db.Plant.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbPlant) {
-      res.json(dbPlant);
-    });
+  // });
 
+  app.put("/api/buy/:id", function (req, res, err) {
+    console.log(req.params.id);
+    db.Plant.update(
+      {
+        sold: true
+      },
+      {
+        returning: true,
+        where: { id: req.params.id }
+      })
+      .then(function(results) {
+        res.json(results);
+      }).catch(err);
   });
+
   
 };
