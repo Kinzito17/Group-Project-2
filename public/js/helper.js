d307af47f6ea8c21f24e18052ef41f7e81379880
Matthew Kinzle <matt.kinzle@gmail.com>
2020-08-04 15:09:00 -0500

    helper again

--- public/js/helper.js
+++ public/js/helper.js
@@ -1,5 +1,3 @@
-
-
 function placePlants(where, plant) {
     console.log(plant);
     let plantCard = $("<div>").attr({ class: "card", "data-id": plant.id, style: "width: 15em;" })
@@ -19,6 +17,33 @@ function placePlants(where, plant) {
 
     let plantDesc = $("<p>").addClass("card-text").text(plant.description);
     $(plantInfo).append(plantDesc);
+
+    $(plantCard).appendTo(where);
+
+}
+
+function readyPlants(where, plant) {
+    console.log(plant);
+    let plantCard = $("<div>").attr({ class: "card", "data-id": plant.id, style: "width: 15em;" })
+    $(where).append(plantCard);
+
+    let cardImg = $("<img>").addClass("card-img-top").attr("src", plant.imgURL);
+    $(plantCard).append(cardImg);
+
+    let plantInfo = $("<div>").addClass("card-body d-flex flex-column");
+    $(plantCard).append(plantInfo);
+
+    let name = $("<h2>").addClass("card-title").text(plant.plantName);
+    $(plantInfo).append(name);
+
+    let plantPrice = $("<h4>").text("$ " + plant.price);
+    $(plantInfo).append(plantPrice);
+
+    let plantDesc = $("<p>").addClass("card-text").text(plant.description);
+    $(plantInfo).append(plantDesc);
+
+    let buyButton = $("<button>").attr({ class: "btn buy-btn mt-auto", "data-id": plant.id }).text("Gimme Green");
+    $(plantInfo).append(buyButton);
 
     $(plantCard).appendTo(where);
 
